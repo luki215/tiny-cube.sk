d55c21fe9cebc99a6bc9625e8c09dede6ed75b4d
Lukáš Březina <lukas@brezinovi.sk>
2023-03-15 18:13:54 +0100

    draft

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    colors: {
+        'green': {
+          '50': '#f0f9f4',
+          '100': '#dbf0e4',
+          '200': '#bae0cb',
+          '300': '#8dc8ad',
+          '400': '#5cab88',
+          '500': '#419d78',
+          '600': '#2a7156',
+          '700': '#215b46',
+          '800': '#1d4839',
+          '900': '#183c2f',
+      },
+    }
+  },
+  plugins: [],
+}
